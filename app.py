c04217f5917eec861ab94e13b07a66d1ba00f8ae
atsushishimazaki <sendatsushimessages@gmail.com>
2018-01-16 11:20:18 +0900

    1

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 def wsgi_app(environ, start_response):
-    status = '200 OK'
-    response_headers = [('Content-type', 'text/plain')]
-    start_response(status, response_headers)
-    response_body = 'Hello World2'
-    yield response_body.encode()
+    # status = '200 OK'
+    # response_headers = [('Content-type', 'text/plain')]
+    # start_response(status, response_headers)
+    # response_body = 'Hello World2'
+    # yield response_body.encode()
 
     # from flask import Flask
-    # from view import api, 0113_input
-    # application = Flask(__name__)
-    # modules_define = [api.app, 0113_input.app]
-    # for app in modules_define:
-    #     application.register_blueprint(app)
+    from view import api, 0113_input
+    application = Flask(__name__)
+    modules_define = [api.app, 0113_input.app]
+    for app in modules_define:
+        application.register_blueprint(app)
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
