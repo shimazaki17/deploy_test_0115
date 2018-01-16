f29187a7554493515df0cba4457c1cd11f2aa5c6
atsushishimazaki <sendatsushimessages@gmail.com>
2018-01-16 11:13:15 +0900

    nineth commit

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ def wsgi_app(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
-
+    response_body = 'Hello World2'
+    yield response_body.encode()
 
     from flask import Flask
     from view import api, 0113_input
